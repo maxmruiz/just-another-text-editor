c3d72526505b380de2cab5468b436b0b3e49f4bc
max <mmfern2003@gmail.com>
2024-02-26 10:36:21 -0500

    logic for installing PWA

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,40 @@
 const butInstall = document.getElementById('buttonInstall');
 
+let deferredPrompt;
+
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+    // Prevent the mini-infobar from appearing on mobile
+    event.preventDefault();
+    
+    // Save the event so it can be triggered later.
+    deferredPrompt = event;
+    
+    // Update UI to notify the user they can install the PWA
+    butInstall.style.display = 'block';
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    if (deferredPrompt) {
+        // Show the install prompt
+        deferredPrompt.prompt();
+        
+        // Wait for the user to respond to the prompt
+        const { outcome } = await deferredPrompt.userChoice;
+        
+        // We've used the prompt, and can't use it again, clear it
+        deferredPrompt = null;
+    }
+    
+    // Hide the install button, the app can't be installed twice
+    butInstall.style.display = 'none';
+});
+
+// Add a handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    // App is installed
+    console.log('PWA has been installed');
+});
