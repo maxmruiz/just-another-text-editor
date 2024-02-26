6a677c8666c5d7f7bc57d163bb6fd8469076e408
max <mmfern2003@gmail.com>
2024-02-26 10:32:58 -0500

    database logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,27 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Logic to add content to the database
+export const putDb = async (content) => {
+  console.log('PUT to the database', content);
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Logic to get all the content from the database
+export const getDb = async () => {
+  console.log('GET from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('🚀 - data fetched from the database', result);
+  return result?.[0]?.value;
+};
 
 initdb();
